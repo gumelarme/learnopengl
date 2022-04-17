1423c39040053aed4c43413c314734dac15be2fc
gumelarme <gumelar.pn@gmail.com>
2022-04-17 17:36:43 +0800

    Feat: drawing rectangle

--- main.cpp
+++ main.cpp
@@ -72,16 +72,30 @@ int main() {
   glDeleteShader(fragmentShader);
 
   // triangle vertices
+  // float vertices[] = {
+  //     -0.5f, -0.5f, 0.0f, // left foot
+  //     0.5f,  -0.5f, 0.0f, // right foot
+  //     0.0f,  0.5f,  0.0f, // tip
+  // };
+
+  // rectangle vertices
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, // left foot
-      0.5f,  -0.5f, 0.0f, // right foot
-      0.0f,  0.5f,  0.0f, // tip
+      -0.5,  0.5f,  0.0f, // top left
+      -0.5f, -0.5f, 0.0f, // bottom left
+      0.5f,  0.5f,  0.0f, // top right
+      0.5f,  -0.5f, 0.0f, // bottom right
+  };
+
+  unsigned int drawIndex[] = {
+      0, 1, 2, // left
+      1, 3, 2, // right
   };
 
   // bind Vertex Array Object
-  unsigned int VAO, VBO;
+  unsigned int VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
   // then configure vertex attributes(s).
@@ -90,6 +104,10 @@ int main() {
   // copy vertices array in a buffert for OpenGL to use
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(drawIndex), drawIndex,
+               GL_STATIC_DRAW);
 
   // linking vertex attribute pointer
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -111,9 +129,10 @@ int main() {
     // draw
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0); // unbind?
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -121,6 +140,7 @@ int main() {
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
   glDeleteProgram(shaderProgram);
 
   glfwTerminate();
